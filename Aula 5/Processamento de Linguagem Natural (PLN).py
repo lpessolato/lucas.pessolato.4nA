2ced7b3c3514a103079ec348df8e22e77a737527
lpessolato <pessolatolucas@gmail.com>
2025-03-18 21:36:39 -0300

    Aula  5 e desafio

---
+++ Aula 5/Processamento de Linguagem Natural (PLN).py
@@ -0,0 +1,13 @@
+texts = [
+    '   Bonjour, comment ca va ?     ',
+    '    Heyyyyy, how are you doing ?   ',
+    '        Hallo, wie gehts ?     '
+]
+texts
+
+
+
+text = "abcd Who is abcd ? That's not a real name!!! abcd"
+text
+
+text.strip('bdac')
